aa15a34f5efaa4dcf94c3289c7861cbd98450010
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-10-28 11:09:58 +0100

    client: document the header file

--- client.h
+++ client.h
@@ -3,9 +3,35 @@
 
 struct client;
 
-int client_init(struct Client *);
-int client_connect(struct Client *, const char *, int);
-int client_send(struct Client *, const char *);
-char* client_recv(struct Client *);
+/**
+ * \brief Initializes the client structure asigning a file descriptor to it
+ */
+int client_init(struct client *);
+
+/**
+ * \brief Connects the client structure to the specified IP and port
+ *
+ * \param client the client structure to connect
+ * \param ip the server IP to connect to
+ * \param port the server port to connect to
+ */
+int client_connect(struct client *, const char *, int);
+
+/**
+ * \brief Send data to the server connected to
+ *
+ * \param client the client structure from which to send the data
+ * \param data the data to send
+ */
+int client_send(struct client *, const char *);
+
+/**
+ * \breif Receive data from the connection made with the client structure
+ *
+ * Blocks the program on a recv until some data is received
+ *
+ * \pram client the client from which to receive data
+ */
+char* client_recv(struct client *);
 
 #endif // CLIENT_H
