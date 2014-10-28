7075533001ccf6737d8f256ffb22d2155d2b2e77
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-10-28 11:17:20 +0100

    server: reorganize code and return 0 on success

--- server.c
+++ server.c
@@ -6,12 +6,16 @@
 #include <netinet/ip.h>
 #include <string.h>
 
-int server_init(struct Server *s) {
-    s->socket = socket(AF_INET, SOCK_STREAM, 0);
-    return s->socket >= 0;
+struct server {
+    void (*callback)(struct server *, int);
+    int socket;
+};
+
+int server_init(struct server *s) {
+    return (s->socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0;
 }
 
-int server_bind(struct Server *s, int port) {
+int server_bind(struct server *s, int port) {
     struct sockaddr_in serv_addr;
 
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -20,29 +24,29 @@ int server_bind(struct Server *s, int port) {
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port        = htons(port);
 
-    if (bind(s->socket, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) != 0) return 0;
+    if (bind(s->socket, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) != 0) return 1;
 
-    return listen(s->socket, 10) == 0;
+    return listen(s->socket, 10);
 }
 
-void server_set_callback(struct Server *s, void (*callback)(struct Server *, int)) {
+void server_set_callback(struct server *s, void (*callback)(struct server *, int)) {
     s->callback = callback;
 }
 
-void server_accept(struct Server *s) {
+void server_accept(struct server *s) {
     int fd = accept(s->socket, (struct sockaddr*)NULL, NULL);
     s->callback(s, fd);
 }
 
-int server_send(struct Server *s, int fd, const char *msg) {
+int server_send(struct server *s, int fd, const char *msg) {
     return send(fd, msg, strlen(msg), 0);
 }
 
-char* server_recv(struct Server *s, int fd) {
+char* server_recv(struct server *s, int fd) {
     int buff_size = sizeof(char) * 1024;
     char *buff = malloc(buff_size);
     memset(buff, 0, buff_size);
-    if (read(fd, buff, buff_size) <= 0) return "\0";
+    if (recv(fd, buff, buff_size, 0) <= 0) return "\0";
     return buff;
 }
 
--- server.h
+++ server.h
@@ -1,16 +1,13 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-struct Server {
-    void (*callback)(struct Server *, int);
-    int socket;
-};
+struct server;
 
-int server_init(struct Server *);
-int server_bind(struct Server *, int);
-void server_set_callback(struct Server *, void (*callback)(struct Server *, int));
-void server_accept(struct Server *);
-int server_send(struct Server *, int, const char *);
-char* server_recv(struct Server *, int);
+int server_init(struct server *);
+int server_bind(struct server *, int);
+void server_set_callback(struct server *, void (*callback)(struct server *, int));
+void server_accept(struct server *);
+int server_send(struct server *, int, const char *);
+char* server_recv(struct server *, int);
 
 #endif // SERVER_H
