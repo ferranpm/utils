1992bbd5d673423e16dec1b60732f37f07eeb6ff
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-12-03 19:56:42 +0100

    pairparser: add tests to check NULL return

--- test/pairparser.h
+++ test/pairparser.h
@@ -3,5 +3,11 @@
 
 int pairparser() {
     struct pairparser *p = pairparser_new("hello=how&are=you&Im=fine&thanks=you", '=', '&');
-    return strcmp(pairparser_get(p, "Im"), "fine") != 0;
+    return
+        strcmp(pairparser_get(p, "hello"), "how") != 0
+        || strcmp(pairparser_get(p, "are"), "you") != 0
+        || strcmp(pairparser_get(p, "Im"), "fine") != 0
+        || strcmp(pairparser_get(p, "thanks"), "you") != 0
+        || pairparser_get(p, "tanks") != NULL;
+        ;
 }
